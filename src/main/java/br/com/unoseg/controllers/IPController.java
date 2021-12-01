bd5e5337e789f1c31ff5fb4b9b2cd241f37f98bc
walterpaulo <walter0paulo@hotmail.com>
2021-12-01 10:19:36 -0300

    put IPController

--- src/main/java/br/com/unoseg/controllers/IPController.java
+++ src/main/java/br/com/unoseg/controllers/IPController.java
@@ -2,9 +2,7 @@ package br.com.unoseg.controllers;
 
 import br.com.unoseg.services.IPService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,6 +15,6 @@ public class IPController {
 
     @GetMapping("")
     public ModelAndView getIP(){
-        return new ModelAndView("homeIP","cidade",ipService.get().getCity());
+        return new ModelAndView("homeIP");
     }
 }
