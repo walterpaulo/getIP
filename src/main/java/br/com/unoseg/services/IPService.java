4679a531491e565f57936ccb7725753ed5c36f68
walterpaulo <walter0paulo@hotmail.com>
2021-12-01 08:58:58 -0300

    put IPService

--- src/main/java/br/com/unoseg/services/IPService.java
+++ src/main/java/br/com/unoseg/services/IPService.java
@@ -2,8 +2,10 @@ package br.com.unoseg.services;
 
 import br.com.unoseg.models.IPModel;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class IPService {
     private String urlAPI = "http://ip-api.com/json/";
 
@@ -24,6 +26,11 @@ public class IPService {
         RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
         restTemplate = restTemplateBuilder.build();
         return restTemplate;
+    }
+
+    public static void main(String[] args) {
+        IPService ipService = new IPService();
+        System.out.println(ipService.get().getCity());
     }
 
 }
