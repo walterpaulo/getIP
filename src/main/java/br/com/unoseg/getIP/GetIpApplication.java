65689ff75149f49ef41be3801263cb3c1e1ea454
walterpaulo <walter0paulo@hotmail.com>
2021-12-01 08:00:41 -0300

    Início do Projeto.

---
+++ src/main/java/br/com/unoseg/getIP/GetIpApplication.java
@@ -0,0 +1,13 @@
+package br.com.unoseg.getIP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetIpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetIpApplication.class, args);
+	}
+
+}
